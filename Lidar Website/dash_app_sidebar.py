6ca3889fd0e47c54c1659e273d45f1d9f835e7f4
Ruben Castaing <castaingruben@gmail.com>
2023-11-21 18:34:18 +1300

    Update dash_app_sidebar.py

--- Lidar Website/dash_app_sidebar.py
+++ Lidar Website/dash_app_sidebar.py
@@ -22,23 +22,20 @@ max_date = df['date'].max()
 
 
 def create_scatter3d_fig(color_scheme='Plasma', OBS_range=(0.019, 2), color_range=[-20, 20], dataframe=df, number_of_rows = 10000):
-    #Include the following line once the OBS is added to the dataframe
     filtered_df = dataframe[(dataframe['obs_signal'] >= OBS_range[0]) & (dataframe['obs_signal'] <= OBS_range[1])] #This line of code repeats itself in most functions.
     fig3d = px.scatter_3d(filtered_df.iloc[:number_of_rows], x='x', y='y', z='z', color='radial_velocity',
                           color_continuous_scale=color_scheme, range_color=color_range, title='Wind Lidar Scan')
-    fig3d.update_layout(autosize=True, scene=dict(aspectmode='data')) #Autosizing the plot
+    fig3d.update_layout(autosize=True, scene=dict(aspectmode='data', xaxis_title='East (m)',  yaxis_title='North (m)', zaxis_title='Height (m)'), coloraxis_colorbar=dict(title='Radial Velocity')) #Autosizing the plot
     fig3d.update_traces(marker={'size': 2})#, hoverinfo='text', hovertext=[...]) #Adding hovertext unsure if the last part is desirable
     return fig3d
 
-def create_scatter3d_animated_fig(color_scheme='Plasma', OBS_range=(0.019, 2), color_range=[-20, 20], dataframe=df):
+def create_scatter3d_animated_fig(color_scheme='Plasma', OBS_range=(0.019, 2), color_range=[-20, 20], dataframe=big_df):
     filtered_df = dataframe[(dataframe['obs_signal'] >= OBS_range[0]) & (dataframe['obs_signal'] <= OBS_range[1])]
     fig3d_animated = px.scatter_3d(filtered_df, x='x', y='y', z='z', color='radial_velocity',
                                    color_continuous_scale=color_scheme, range_color=color_range, title='Wind Lidar Scan Animated',
                                    animation_frame='time_step')
-    fig3d_animated.update_layout(autosize=True, scene=dict(
-        xaxis=dict(range=[-1000, 1000]),
-        yaxis=dict(range=[-1000, 1000]),
-        zaxis=dict(range=[0, 200]),
+    fig3d_animated.update_layout(autosize=True, coloraxis_colorbar=dict(title='Radial Velocity'), scene=dict(xaxis_title='East (m)',  yaxis_title='North (m)', zaxis_title='Height (m)',
+        xaxis=dict(range=[-1000, 1000]), yaxis=dict(range=[-1000, 1000]), zaxis=dict(range=[0, 200]),
         aspectmode='manual',
         aspectratio=dict(x=1, y=1, z=0.1)
     ))
@@ -62,15 +59,27 @@ def create_time_series_fig(OBS_range, dataframe=big_df):
     return fig
 
 # Create the heatmap figure
-def create_heat_map(color_scheme='Plasma', OBS_range=(0.019, 2), dataframe=big_df):
+#This creates the varience in funtion which will be extra slow
+def create_heat_map(color_scheme='Plasma', OBS_range=(0.019, 2), color_range=[-20, 20], dataframe=big_df, attribute='radial_velocity'):
     filtered_df = dataframe[(dataframe['obs_signal'] >= OBS_range[0]) & (dataframe['obs_signal'] <= OBS_range[1])]
-    aggregated_df = filtered_df.groupby(['distance', 'time'])['radial_velocity'].mean().reset_index()    # Aggregate the data
-    heatmap_df = aggregated_df.pivot(index="distance", columns="time", values="radial_velocity")
+
+    if attribute == 'variance':
+        # Calculate the variance and explicitly name the column
+        aggregated_df = filtered_df.groupby(['distance', 'time'])['radial_velocity'].var().reset_index(name='variance')
+        attribute = 'variance'  # Set the attribute to the new column name
+    else:
+        aggregated_df = filtered_df.groupby(['distance', 'time'])['radial_velocity'].mean().reset_index(name='radial_velocity')
+
+    heatmap_df = aggregated_df.pivot(index="distance", columns="time", values=attribute)
+
     fig = px.imshow(heatmap_df, 
-                    labels=dict(x="time", y="distance", color="radial_velocity"), 
-                    origin='lower', title='Daily Vertical Stare',
-                    color_continuous_scale = color_scheme)
+                    labels=dict(x="Time", y="Distance", color=attribute.title()), 
+                    origin='lower', title=f'Daily Vertical Stare - {attribute.title()}',
+                    color_continuous_scale=color_scheme, range_color=color_range)
     return fig
+
+
+
 
 # Initialize the Dash app
 app = Dash(__name__)
@@ -117,7 +126,7 @@ app.layout = html.Div([
                     max=1,
                     step=0.01,
                     value=[0.1, 1],  # Default range
-                    marks={i: '{:.1f}'.format(i) for i in np.arange(0, 1.1, 0.2)},
+                    marks = {i: '{:.1f}'.format(i) for i in [x * 0.2 for x in range(6)]},
             ),
             html.H5("Color Range"),
                 dcc.RangeSlider(
@@ -136,6 +145,16 @@ app.layout = html.Div([
                     start_date=min_date,
                     end_date=max_date,
             ),
+            html.H5("Varience or Velocity"),
+                dcc.Dropdown(
+                    id='attribute-dropdown',
+                        options=[
+                            {'label': 'Radial Velocity', 'value': 'radial_velocity'},
+                            {'label': 'Variance', 'value': 'variance'}],
+                        value='radial_velocity'  # Default value
+            ),
+
+
         ], style={'width': '20%', 'padding': '20px', 'backgroundColor': '#f2f2f2'})
     ], style={'display': 'flex'})
 
@@ -147,14 +166,16 @@ app.layout = html.Div([
      Output('heatmap-plot', 'figure')],
     [Input('color-scheme-dropdown', 'value'),
      Input('OBS-range-slider', 'value'),
-     Input('color-range-slider', 'value')]  # Include the color range slider input
+     Input('color-range-slider', 'value'),
+     Input('attribute-dropdown', 'value')]  # Add the new attribute dropdown input
 )
-def update_plots(color_scheme, OBS_range, color_range):
+def update_plots(color_scheme, OBS_range, color_range, selected_attribute):
     scatter3d_fig = create_scatter3d_fig(color_scheme, OBS_range, color_range=color_range)
     scatter3d_animated_fig = create_scatter3d_animated_fig(color_scheme, OBS_range, color_range=color_range)
     time_series_fig = create_time_series_fig(OBS_range)
-    heatmap_fig = create_heat_map(color_scheme, OBS_range)
+    heatmap_fig = create_heat_map(color_scheme, OBS_range, color_range, attribute=selected_attribute)  # Pass the selected attribute
     return scatter3d_fig, scatter3d_animated_fig, time_series_fig, heatmap_fig
+
 
 
 # Run the app
